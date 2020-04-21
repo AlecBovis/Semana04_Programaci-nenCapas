2f856c4bad3be39542ac97b84354676adf9f313f
AlecBovis <alec.bovis@tecsup.edu.pe>
2020-04-20 21:40:34 -0500

    Agregar archivos de proyecto.

---
+++ Data/DDetallePedido.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using Entity;
+
+namespace Data
+{
+    public class DDetallePedido
+    {
+        public List<DetallePedido> GetDetallePedidos(DetallePedido detallePedido)
+        {
+            SqlParameter[] parameters = null;
+            string comandText = string.Empty;
+            List<DetallePedido> detallepedidos = null;
+
+            try
+            {
+                comandText = "USP_ListarDetalle";
+                parameters = new SqlParameter[1];
+                parameters[0] = new SqlParameter("@IdPedido", SqlDbType.Int);
+                parameters[0].Value = detallePedido.Pedido.IdPedido;
+                detallepedidos = new List<DetallePedido>();
+
+                using (SqlDataReader reader = SQLHelper.ExecuteReader(SQLHelper.Connection, "USP_ListarDetalle"
+                    , CommandType.StoredProcedure, parameters))
+                {
+                    while (reader.Read())
+                    {
+                        detallepedidos.Add(new DetallePedido
+                        {
+                            IdPedido = reader["idpedido"] != null ? Convert.ToInt32(reader["idpedido"]) : 0 ,
+                            IdProducto = reader["idproducto"] != null ? Convert.ToInt32(reader["idproducto"]) : 0,
+                            PrecioUnidad = reader["preciounidad"] != null ? Convert.ToDecimal(reader["preciounidad"]) : 0,
+                            Cantidad = reader["cantidad"] != null ? Convert.ToInt32(reader["cantidad"]) : 0,
+                            Descuento = reader["descuento"] != null ? Convert.ToDecimal(reader["descuento"]) : 0,
+                        }) ;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return detallepedidos;
+        }
+    }
+}
